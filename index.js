636765c3b8142523209aa2f7f2f3b0774848d750
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-06-20 03:13:18 -0400

    Updating with pinetree

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+let greenCharacter = document.createElement("img");
+greenCharacter.src = "assets/green-character.gif";
+greenCharacter.style.position = "fixed";
+greenCharacter.style.left = "100px";
+greenCharacter.style.bottom = "100px";
+document.body.append(greenCharacter);
+
+// adding a pine tree
+let pineTree = document.createElement("img");
+pineTree.src = "assets/pine-tree.png";
+pineTree.style.position = "fixed";
+pineTree.style.left = "450px";
+pineTree.style.bottom = "200px";
+document.body.append(pineTree);
