80a84429962d7f185d50ad464a742169ab3d31e5
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-06-20 03:50:26 -0400

    updated js with sword

--- index.js
+++ index.js
@@ -31,3 +31,15 @@ newImage(pathCrate, "150px", "200px");
 // adding well image
 pathWell = "assets/well.png";
 newImage(pathWell, "500px", "425px");
+
+// adding sword
+let sword = document.createElement("img");
+sword.src = "assets/sword.png";
+sword.position = "fixed";
+sword.left = "500px";
+sword.bottom = "405px";
+document.body.append(sword);
+
+sword.addEventListener("click", function () {
+  sword.remove();
+});
